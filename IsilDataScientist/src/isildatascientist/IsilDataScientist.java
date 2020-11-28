bc222f73d4fc04733e46c3a41d316aec1725c0b9
Thomas <walibifan@hotmail.be>
2020-11-28 15:43:54 +0100

    Rename base projet

--- IsilDataScientist/src/isildatascientist/IsilDataScientist.java
+++ IsilDataScientist/src/isildatascientist/IsilDataScientist.java
@@ -3,20 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package helloworld;
+package isildatascientist;
 
 /**
  *
  * @author Thomas
  */
-public class HelloWorld {
+public class IsilDataScientist {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World !");
+        System.out.println("Hello World");
     }
     
 }
